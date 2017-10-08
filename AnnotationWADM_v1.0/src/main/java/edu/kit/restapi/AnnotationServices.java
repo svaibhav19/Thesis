9cfed7ce135a40ad71cd30954a3a7fb32b122734
svaibhav19 <sakharkarvv@gmail.com>
2017-10-08 16:37:26 +0200

    added services to annotation serive class

--- AnnotationWADM_v1.0/src/main/java/edu/kit/restapi/AnnotationServices.java
+++ AnnotationWADM_v1.0/src/main/java/edu/kit/restapi/AnnotationServices.java
@@ -15,6 +15,8 @@ import org.openrdf.repository.config.RepositoryConfigException;
 
 import edu.kit.api.QueryByTarget;
 import edu.kit.api.QueryByTargetImpl;
+import edu.kit.api.json.AnnotationGenerator;
+import edu.kit.api.json.AnnotationGeneratorImpl;
 import edu.kit.api.json.JsonMapper;
 import edu.kit.api.json.JsonMapperImp;
 import edu.kit.api.page.PageAnnotationGenerator;
@@ -47,21 +49,21 @@ public class AnnotationServices {
 	 * @throws AnnotationExceptions
 	 */
 	@POST
-	@Path("store")
+	@Path("postToAnnoStore")
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-	public String checkJsonLD(@Context HttpHeaders headers,@QueryParam("digitalObjID") String digitalObjID, String dataToStore)
-			throws AnnotationExceptions {
+	public String postToAnnoStore(@Context HttpHeaders headers, @QueryParam("digitalObjID") String digitalObjID,
+			String dataToStore) throws AnnotationExceptions {
 		String mediaType = headers.getRequestHeaders().get("content-type").get(0);
-		
+
 		try {
-			if(mediaType.equals("application/xml")){
+			if (mediaType.equals("application/xml")) {
 				PageAnnotationGenerator generator = new PageAnnotationGeneratorImpl();
 				return generator.parseAnnotations(digitalObjID, dataToStore);
-			}else if(mediaType.equals("application/json")){
+			} else if (mediaType.equals("application/json")) {
 				JsonMapper jsonMapper = new JsonMapperImp();
 				return jsonMapper.parseJson(dataToStore);
 			}
-			
+
 		} catch (RepositoryException e) {
 			throw new AnnotationExceptions(e.getMessage(), StatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
 		} catch (RepositoryConfigException e) {
@@ -76,19 +78,11 @@ public class AnnotationServices {
 
 	/**
 	 * This method is used to run any query (select / construct ) and obtain the
-	 * results in required format. Which can be any of the following,
-	 * "TURTLE" TURTLE 
-	 * "TTL" TURTLE 
-	 * "Turtle" TURTLE
-	 * "N-TRIPLES" NTRIPLES 
-	 * "N-TRIPLE" NTRIPLES 
-	 * "NT" NTRIPLES 
-	 * "JSON-LD" JSONLD
-	 * "RDF/XML-ABBREV" RDFXML 
-	 * "RDF/XML" RDFXML_PLAIN 
-	 * "N3" N3 
-	 * "RDF/JSON" RDFJSON
-	 * "ld+json" anno-json annotation profile
+	 * results in required format. Which can be any of the following, "TURTLE"
+	 * TURTLE "TTL" TURTLE "Turtle" TURTLE "N-TRIPLES" NTRIPLES "N-TRIPLE"
+	 * NTRIPLES "NT" NTRIPLES "JSON-LD" JSONLD "RDF/XML-ABBREV" RDFXML "RDF/XML"
+	 * RDFXML_PLAIN "N3" N3 "RDF/JSON" RDFJSON "ld+json" anno-json annotation
+	 * profile
 	 * 
 	 * @param queryString
 	 * @param format
@@ -105,7 +99,8 @@ public class AnnotationServices {
 	}
 
 	/**
-	 * This Method is used to get Single annotaion by the given ID. 
+	 * This Method is used to get Single annotaion by the given ID.
+	 * 
 	 * @param annoid
 	 * @return
 	 */
@@ -118,4 +113,31 @@ public class AnnotationServices {
 		return results;
 	}
 
+	/**
+	 * 
+	 * @param format
+	 * @param dataToStore
+	 * @return String
+	 * @throws AnnotationExceptions
+	 */
+	@POST
+	@Path("postRDFToAnnoStore")
+	@Consumes(MediaType.APPLICATION_XML)
+	public String postRDFToAnnoStore(@QueryParam("format") String format, String dataToStore) throws AnnotationExceptions {
+		AnnotationGenerator annoGen = new AnnotationGeneratorImpl();
+		try {
+			if (format.equalsIgnoreCase("RDF/XML")) {
+				return annoGen.validateNStoreRDFXML(dataToStore);
+			} else if (format.equalsIgnoreCase("JSON-LD")) {
+				return annoGen.validateNStoreJSONLD(dataToStore);
+			} else if (format.equalsIgnoreCase("TURTLE")) {
+				return annoGen.validateNStoreTURTLE(dataToStore);
+			} else if (format.equalsIgnoreCase("N-TRIPLE")) {
+				return annoGen.validateNStoreNTriple(dataToStore);
+			}
+		} catch (AnnotationExceptions e) {
+			throw new AnnotationExceptions(e.getMessage(), StatusCode.BAD_REQUEST.getStatusCode());
+		}
+		return "Invalid Format";
+	}
 }
